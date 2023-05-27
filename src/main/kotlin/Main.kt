a5f61f8116d77ddfb119e3f0058d8e2f694ac034
Tudor Dascalu <tudordascalu1@gmail.com>
2023-05-27 15:06:22 +0200

    Removes saving to file of publications

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,6 @@ import kotlinx.serialization.builtins.ListSerializer
 import model.Publication
 import org.slf4j.LoggerFactory
 import services.ScholarScraperService
-import java.io.File
-import java.io.IOException
 
 fun main(args: Array<String>) {
     val logger = LoggerFactory.getLogger("MainLogger")
@@ -15,13 +13,6 @@ fun main(args: Array<String>) {
     val publications = scholarScraperService.getPublications(authorIds)
     // Serialize publications
     val publicationsSerialized = Yaml.default.encodeToString(ListSerializer(Publication.serializer()), publications)
-    // Save publications
-    try {
-        val file = File("/app/publications.yaml")
-        file.writeText(publicationsSerialized)
-    } catch (e: IOException) {
-        e.printStackTrace()
-    }
     // Log publications
     print(publicationsSerialized)
 }
